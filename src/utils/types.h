8bc2a6f0d815369c6c196aab8b772f1ec0b70da5
niushijia <niushijia@kylinos.cn>
2026-04-04 09:55:42 +0800

    add(utils): 增加ipc相关数据结构
    增加用来发给ui侧消息的payload结构体
    
    Task: #605565
    From: kylin
    Severity: Moderate

---
+++ src/utils/types.h
@@ -0,0 +1,60 @@
+#ifndef _FREEWB_UTILS_TYPES_H_
+#define _FREEWB_UTILS_TYPES_H_
+
+#include <string>
+#include <vector>
+
+namespace freewb
+{
+
+enum LayoutType
+{
+    Horizontal = 1,
+    Vertical
+};
+
+struct SpotRectPayload
+{
+    int x = 0;
+    int y = 0;
+    int w = 0;
+    int h = 0;
+};
+
+struct CandidatePayload
+{
+    std::vector<std::string> labels;
+    std::vector<std::string> texts;
+    std::vector<std::string> attrs;
+    bool hasPrev = false;
+    bool hasNext = false;
+    int cursor = -1;
+    LayoutType layout = Horizontal;
+};
+
+struct CandidatePreeditPayload
+{
+    std::string text;
+    int caret = 0;
+    bool show = false;
+};
+
+struct CandidateAuxPayload
+{
+    std::string text;
+    bool show = false;
+};
+
+struct ToolbarPropertiesPayload
+{
+    std::string uniqueName;
+    std::string name;
+    std::string shortDescription;
+    std::string longDescription;
+
+    bool active = false;
+};
+
+} // namespace freewb
+
+#endif
