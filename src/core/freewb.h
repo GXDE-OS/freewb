976aec18c860e16d6979882bc395f2b7301dac77
niushijia <niushijia@kylinos.cn>
2026-04-04 10:25:16 +0800

    fix(core): 修改接口声明
    添加const
    
    Task: #605565
    From: kylin
    Severity: Moderate

--- src/core/freewb.h
+++ src/core/freewb.h
@@ -7,8 +7,8 @@ class IFreewb
 {
 public:
     virtual ~IFreewb() = default;
-    virtual const char *name() = 0;
-    virtual bool available() = 0;
+    virtual const char *name() const = 0;
+    virtual bool available() const = 0;
     virtual void changeAvailable() = 0;
 };
 } // namespace freewb
