a82e0373ada43155c0f59054882420f6d7c22350
hantengc <hanteng@kylinos.cn>
2026-05-21 10:44:44 +0800

    feat: 支持输入法框架fcitx4
    1. 完成fcitx4支持
    
    Task: #605565
    From: kylin
    Severity: Moderate

---
+++ src/fcitx4/module.cpp
@@ -0,0 +1,196 @@
+#ifndef FCITX4_MODULE_H
+#define FCITX4_MODULE_H
+
+#include <cstring>
+#include <string>
+
+#include <dbus/dbus.h>
+#include <fcitx/ime.h>
+#include <fcitx/instance.h>
+#include <fcitx/context.h>
+#include <fcitx/module.h>
+#include <fcitx/hook.h>
+#include <fcitx/keys.h>
+#include <fcitx/module/dbus/fcitx-dbus.h>
+
+#include "freewb.h"
+#include "idbus.h"
+#include "libdbus_proxy.h"
+#include "types.h"
+#include "config.h"
+
+typedef struct
+{
+    freewb::Freewb *freewb_;
+    FcitxInstance *fcitxInstance_;
+} freewb_fcitx4_imclass;
+
+static void *FreewbIMCreate(FcitxInstance *instance);
+static void FreewbIMDestroy(void *arg);
+static boolean FreewbIMInit(void *arg);
+static void FreewbIMReset(void *arg);
+static INPUT_RETURN_VALUE FreewbIMDoInput(void *arg, FcitxKeySym sym, unsigned int state);
+static INPUT_RETURN_VALUE FreewbIMDoReleaseInput(void *arg, FcitxKeySym sym, unsigned int state);
+static void FreewbIMOnChanged(void *arg);
+
+static void updateCursorPosition(freewb_fcitx4_imclass *imclass);
+
+#ifdef __cplusplus
+extern "C"
+{
+#endif
+    FCITX_DEFINE_PLUGIN(fcitx_freewb, ime, FcitxIMClass) = {
+        FreewbIMCreate,
+        FreewbIMDestroy
+    };
+#ifdef __cplusplus
+}
+#endif
+
+static boolean FreewbIMInit(void *arg)
+{
+    return true;
+}
+
+static void FreewbIMReset(void *arg)
+{
+    freewb_fcitx4_imclass *imclass = static_cast<freewb_fcitx4_imclass *>(arg);
+    if (imclass == nullptr || imclass->freewb_ == nullptr)
+    {
+        return;
+    }
+
+    imclass->freewb_->reset();
+}
+
+static INPUT_RETURN_VALUE FreewbIMDoInput(void *arg, FcitxKeySym sym, unsigned int state)
+{
+    freewb_fcitx4_imclass *imclass = static_cast<freewb_fcitx4_imclass *>(arg);
+    if (imclass == nullptr || imclass->freewb_ == nullptr)
+    {
+        return IRV_TO_PROCESS;
+    }
+
+    updateCursorPosition(imclass);
+
+    const bool processed = imclass->freewb_->processKey(static_cast<FreewbKeySym>(sym), static_cast<FreewbKeyState>(state));
+    if (processed)
+    {
+        imclass->freewb_->updateCandidateAndPreeditToUI();
+        return IRV_DO_NOTHING;
+    }
+    return IRV_TO_PROCESS;
+}
+
+static INPUT_RETURN_VALUE FreewbIMDoReleaseInput(void *arg, FcitxKeySym sym, unsigned int state)
+{
+    FCITX_UNUSED(arg);
+    FCITX_UNUSED(sym);
+    FCITX_UNUSED(state);
+    return IRV_TO_PROCESS;
+}
+
+static void FreewbIMOnChanged(void *arg)
+{
+    freewb_fcitx4_imclass *imclass = static_cast<freewb_fcitx4_imclass *>(arg);
+    if (imclass == nullptr || imclass->freewb_ == nullptr || imclass->fcitxInstance_ == nullptr)
+    {
+        return;
+    }
+    
+    FcitxIM *im = FcitxInstanceGetCurrentIM(imclass->fcitxInstance_);
+    if (im == nullptr)
+    {
+        return;
+    }
+    
+    const char *im_name = im->uniqueName;
+    if (im_name == nullptr)
+    {
+        return;
+    }
+
+    if (strncmp(im_name, "freewb", sizeof("freewb")) == 0)
+    {
+        FcitxLog(INFO, "will activate freewb and show ui.");  
+        FREEWB_DEBUG("will activate freewb and show ui.");
+        FcitxUISetStatusVisable(imclass->fcitxInstance_, _("settings"), true);
+        imclass->freewb_->activate();
+    }
+    else
+    {
+        FcitxLog(INFO, "will deactivate freewb and hide ui.");
+        FREEWB_DEBUG("will deactivate freewb and hide ui.");
+        FcitxUISetStatusVisable(imclass->fcitxInstance_, _("settings"), false);
+        imclass->freewb_->deactivate();
+    }
+}
+
+void *FreewbIMCreate(FcitxInstance *instance)
+{
+    DBusConnection *dbusConnection = FcitxDBusGetConnection(instance);
+    freewb_fcitx4_imclass *imclass =
+        static_cast<freewb_fcitx4_imclass *>(fcitx_utils_malloc0(sizeof(freewb_fcitx4_imclass)));
+    imclass->freewb_ = nullptr;
+    imclass->fcitxInstance_ = instance;
+
+    auto *idbus = new ::freewb::ipc::LibDbusProxy(dbusConnection);
+    imclass->freewb_ = new freewb::Freewb(idbus, [imclass](const std::string &text) {
+        FcitxInputContext *ic = FcitxInstanceGetCurrentIC(imclass->fcitxInstance_);
+        if (ic != nullptr)
+        {
+            FcitxInstanceCommitString(imclass->fcitxInstance_, ic, text.c_str());
+        }
+    });
+
+    FcitxIMEventHook imhook = {FreewbIMOnChanged, imclass};
+	FcitxInstanceRegisterIMChangedHook(instance, imhook);
+
+    FcitxIMIFace iface;
+    memset(&iface, 0, sizeof(FcitxIMIFace));
+    iface.Init = FreewbIMInit;
+    iface.ResetIM = FreewbIMReset;
+    iface.DoInput = FreewbIMDoInput;
+    iface.DoReleaseInput = FreewbIMDoReleaseInput;
+
+    FcitxInstanceRegisterIMv2(instance, imclass, "freewb", "freewb", "freewb", iface, 10, "zh_CN");
+
+    return imclass;
+}
+
+void FreewbIMDestroy(void *arg)
+{
+    freewb_fcitx4_imclass *imclass = static_cast<freewb_fcitx4_imclass *>(arg);
+    if (imclass == nullptr)
+    {
+        return;
+    }
+
+    if (imclass->freewb_ != nullptr)
+    {
+        delete imclass->freewb_;
+        imclass->freewb_ = nullptr;
+    }
+
+    fcitx_utils_free(imclass);
+}
+
+static void updateCursorPosition(freewb_fcitx4_imclass *imclass)
+{
+    if (imclass == nullptr || imclass->freewb_ == nullptr || imclass->fcitxInstance_ == nullptr)
+    {
+        return;
+    }
+
+    ::freewb::SpotRectPayload spotRect = {0, 0, 0, 0};
+    FcitxInputContext *ic = FcitxInstanceGetCurrentIC(imclass->fcitxInstance_);
+    if (ic == nullptr)
+    {
+       return;
+    }
+
+    FcitxInstanceGetWindowRect(imclass->fcitxInstance_, ic, &spotRect.x, &spotRect.y, &spotRect.w, &spotRect.h);
+    imclass->freewb_->dbusProxy()->callPanelUpdateSpotRect(spotRect);
+}
+
+#endif
