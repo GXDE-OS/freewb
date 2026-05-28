cf1c8cc1d8214e49116f126e9268e6863f353179
hantengc <hanteng@kylinos.cn>
2026-05-28 16:14:20 +0800

    refactor: 删除繁体转简体相关的功能
    目前不涉及繁体转简体相关的工作
    
    Task: #605565
    From: kylin
    Severity: Moderate

--- src/core/chttrans.cpp
+++ src/core/chttrans.cpp
@@ -9,7 +9,7 @@ namespace freewb
 
 void Chttrans::loadPair(const std::string &s2tProfile, const std::string &t2sProfile)
 {
-    const std::string s2tPath = s2tProfile.empty() ? std::string(FREEWB_INSTALL_PKGDATADIR "/data/s2t.json") : s2tProfile;
+    const std::string s2tPath = s2tProfile.empty() ? std::string(FREEWB_INSTALL_PKGDATADIR "/data/s2t/s2t.json") : s2tProfile;
     try
     {
         s2t_ = std::make_unique<opencc::SimpleConverter>(s2tPath);
@@ -18,17 +18,6 @@ void Chttrans::loadPair(const std::string &s2tProfile, const std::string &t2sPro
     {
         FREEWB_ERROR("Failed to load s2t converter: {}", s2tPath);
         s2t_.reset();
-    }
-
-    const std::string t2sPath = t2sProfile.empty() ? std::string(FREEWB_INSTALL_PKGDATADIR "/data/t2s.json") : t2sProfile;
-    try
-    {
-        t2s_ = std::make_unique<opencc::SimpleConverter>(t2sPath);
-    }
-    catch (const std::exception &)
-    {
-        FREEWB_ERROR("Failed to load t2s converter: {}", t2sPath);
-        t2s_.reset();
     }
 }
 
@@ -67,23 +56,6 @@ void Chttrans::simpToTrad(std::string &text) const
     catch (const std::exception &)
     {
         FREEWB_ERROR("Failed to convert text to trad: {}", text);
-    }
-}
-
-void Chttrans::tradToSimp(std::string &text) const
-{
-    if (!available_ || !t2s_)
-    {
-        return;
-    }
-
-    try
-    {
-        text = t2s_->Convert(text);
-    }
-    catch (const std::exception &)
-    {
-        FREEWB_ERROR("Failed to convert text to simp: {}", text);
     }
 }
 
--- src/core/chttrans.h
+++ src/core/chttrans.h
@@ -22,14 +22,12 @@ public:
     void changeAvailable() override;
 
     void simpToTrad(std::string &text) const;
-    void tradToSimp(std::string &text) const;
 
 private:
     void loadPair(const std::string &s2tProfile, const std::string &t2sProfile);
 
 private:
     std::unique_ptr<opencc::SimpleConverter> s2t_;
-    std::unique_ptr<opencc::SimpleConverter> t2s_;
     bool available_ = true;
 };
 
