44b06240c8fc99ba847ba90756512221fa90b866
hantengc <hanteng@kylinos.cn>
2026-06-18 10:19:03 +0800

    fix: 修改输入法dbus名称
    1. 输入法dbus服务名称修改成org.freewb，对应freewb.org网站

--- src/ipc/ipc.h
+++ src/ipc/ipc.h
@@ -2,15 +2,15 @@
 #define IPC_H
 
 /***************　极点五笔输入法UI dbus服务名　*****************/
-#define FREEWUBI_PANEL_SERVICENAME "org.ukui.freewubi.panel"
-#define FREEWUBI_PANEL_OBJECTPATH "/org/ukui/freewubi/panel"
-#define FREEWUBI_PANEL_INTERFACE "org.ukui.freewubi.panel"
-#define FREEWUBI_PANEL_BUSNAME "freewubi_panel_bus"
+#define FREEWUBI_PANEL_SERVICENAME "org.freewb.panel"
+#define FREEWUBI_PANEL_OBJECTPATH "/org/freewb/panel"
+#define FREEWUBI_PANEL_INTERFACE "org.freewb.panel"
+#define FREEWUBI_PANEL_BUSNAME "freewb_panel_bus"
 
 /***************　极点五笔输入法设置 dbus服务名　*****************/
-#define FREEWUBI_SETTINGS_SERVICENAME "org.ukui.freewubi.settings"
-#define FREEWUBI_SETTINGS_OBJECTPATH "/org/ukui/freewubi/settings"
-#define FREEWUBI_SETTINGS_INTERFACE "org.ukui.freewubi.settings"
-#define FREEWUBI_SETTINGS_BUSNAME "freewubi_settings_bus"
+#define FREEWUBI_SETTINGS_SERVICENAME "org.freewb.settings"
+#define FREEWUBI_SETTINGS_OBJECTPATH "/org/freewb/settings"
+#define FREEWUBI_SETTINGS_INTERFACE "org.freewb.settings"
+#define FREEWUBI_SETTINGS_BUSNAME "freewb_settings_bus"
 
 #endif
